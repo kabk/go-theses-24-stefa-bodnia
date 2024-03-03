283e372e9f95918f245da75b3945b06414507aae
Stefaniia Bodnia <81180702+StefaBodnia@users.noreply.github.com>
2024-03-03 15:01:21 +0100

    update 0303

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,94 +2,127 @@ const folder = "./img/visualessay/";
 
 const images = [
   {
-    name: "1.jpeg",
-    size: 0.5,
-  },
-  {
-    name: "2.jpg",
+    name: " 1.png",
     size: 1,
+    caption: "A miner standing with his back to the viewer, holding a light bulb. Etude to the painting Coal Miners. Shift [Шахтар, що стоїть спиною до глядача, з лампочкою в руках. Етюд до картини “Вуглекопи. Зміна”], Nikolay Kasatkin, oil on canvas, 1895",
   },
   {
-    name: "3.jpg",
-    size: 0.5,
+    name: "1 .png",
+    size: 0.15,
+    caption: "1 Kopiika, 1992. Coin was produced in Luhansk, Ukraine",
   },
   {
-    name: "4.jpg",
+    name: "1.png",
+    size: 1,
+    caption: "Abandoned People, Valerii Miloserdov, [Покинуті люди, Валерій Милосердов], 1994-1999.",
+  },
+  {
+    name: " 2.png",
+    size: 1,
+    caption: "Abandoned People, Valerii Miloserdov, [Покинуті люди, Валерій Милосердов], 1994-1999",
+  },
+  {
+    name: " 3.png",
+    size: 1,
+    caption: "The Chimborazo seen from the Plateau of Tapia, Alexander Von Humboldt, the first scholar to link colonialism to environmental degradation and the subjugation of local communities",
+  },
+  {
+    name: " 4.png",
     size: 0.8,
+    caption: "6test",
   },
   {
     name: "5.jpg",
-    size: 0.3,
+    size: 0.7,
+    caption: "7test",
   },
   {
-    name: "6.jpg",
+    name: " 6.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "7.jpg",
+    name: " 7.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "8.jpeg",
+    name: " 8.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "9.jpg",
+    name: " 9.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "10.jpg",
+    name: " 10.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "11.jpg",
+    name: " 11.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "12.jpg",
+    name: " 12.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "13.jpg",
+    name: " 13.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "14.png",
+    name: " 14.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "15.jpg",
+    name: " 15.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "16.png",
+    name: " 16.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "17.jpg",
+    name: " 17.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "18.png",
+    name: " 18.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "19.jpg",
+    name: " 19.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "20.jpg",
+    name: " 20.png",
     size: 1,
+    caption: "test",
   },
   {
-    name: "21.png",
+    name: " 21.png",
     size: 1,
+    caption: "test",
   },
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
   const slider = document.getElementById("slider");
   const sliderContainer = document.getElementById("slider-container");
+  const captionsBlock = document.getElementsByClassName('captions')[0];
+
   let currentIndex = 0;
 
   const insertImages = () => {
@@ -97,15 +130,13 @@ document.addEventListener("DOMContentLoaded", () => {
       const img = document.createElement("img");
       img.src = folder + image.name;
       slider.appendChild(img);
-      img.style.height = image.size * 100 + '%';
+      img.style.height = image.size * 100 + "%";
     });
-  }
-
-  insertImages();
-
+  };
+  
   const updateSlider = () => {
-    console.log(currentIndex);
-
+    const caption = images[currentIndex].caption;
+    captionsBlock.innerHTML = caption;
     const imagesInSlider = slider.children;
     let leftBorderCoordinate = 0;
     for (let i = 0; i < currentIndex; i++) {
@@ -114,20 +145,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const newPosition = -leftBorderCoordinate + "px";
 
-    slider.style.transform = "translateX(" + newPosition + ")"
+    slider.style.transform = "translateX(" + newPosition + ")";
   };
 
+
+  insertImages();
+  updateSlider()
+
+  
   const prevSlide = () => {
     if (currentIndex !== 0) {
       currentIndex--;
       updateSlider();
+      console.log(images[currentIndex]);
     }
   };
 
   const nextSlide = () => {
     currentIndex++;
     updateSlider();
-    if ((currentIndex-1)  % images.length === 0) {
+    console.log(images[currentIndex]);
+
+    if ((currentIndex - 1) % images.length === 0) {
       insertImages();
     }
   };
